8debc225995587b474374dbf573f577d1cac36bd
ekta76 <mirekmerta2005@gmail.com>
2023-05-08 15:31:12 +0200

    Updated start and end screen

--- My project/Assets/Scripts/EndMenu.cs
+++ My project/Assets/Scripts/EndMenu.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndMenu : MonoBehaviour
 {
     
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene("Start Screen");
+    }
+
     public void Quit()
     {
         Application.Quit();
