7328b7762ed8025bdfff0bc8498b78c3689c8a92
ekta76 <mirekmerta2005@gmail.com>
2023-05-11 20:30:59 +0200

    Game completed

---
+++ My project/Assets/Scripts/ObjectMovement_platform.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement_platform : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    private int currentWaypointIndex = 0;
+
+    [SerializeField] private float speed = 2f;
+
+    private float time = 0f;
+
+
+    private void Update()
+    {
+        time++;
+
+        if (time >= 2500)
+        {
+            if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
+            {
+                currentWaypointIndex++;
+                if (currentWaypointIndex >= waypoints.Length)
+                {
+                    currentWaypointIndex = 0;
+                }
+            }
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
+        }
+
+    }
+
+}
